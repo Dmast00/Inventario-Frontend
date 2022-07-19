03ee32de9f2719139d99bbf23caa42acc0bf6592
Dmast00 <obed.hinojosa@outlook.com>
2022-07-19 13:10:44 -0500

    Se agrego el ts de salidas con el html y css a medias.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductosComponent } from "./productos/productos.component";
 import { CategoriasComponent } from "./categorias/categorias.component";
 import { InventarioComponent } from "./inventario/inventario.component";
+import { SalidasComponent } from "./salidas/salidas.component";
 
 const routes: Routes = [
   {path:'Productos',component:ProductosComponent},
   {path:'Categorias',component:CategoriasComponent},
-  {path:'Inventario',component:InventarioComponent}
+  {path:'Inventario',component:InventarioComponent},
+  {path:'Salidas',component:SalidasComponent}
 ];
 
 @NgModule({
